0a59562e188dfee2278c1507d43ed05c26f1c546
Vova4o <gavrilenko2012@gmail.com>
2024-03-16 19:02:23 +0800

    lets see if agent works

--- internal/app/agent.go
+++ internal/app/agent.go
@@ -5,11 +5,27 @@ import (
 
 	allflags "Vova4o/metrix/internal/flag"
 	clientmetrics "Vova4o/metrix/internal/handlers/client"
+
+	"github.com/go-resty/resty/v2"
 )
 
 func NewAgent() {
+	// Create a new resty client
+	client := resty.New()
 
-	//Start the cicle of collecting and sending metrics
+	// Create a new MetricsCollector
+	mc := &clientmetrics.MetricsCollector{
+		GaugeMetrics:   make(map[string]float64),
+		CounterMetrics: make(map[string]int),
+	}
+
+	// Create a new MetricsAgent
+	ma := &clientmetrics.MetricsAgent{
+		MetricsCollector: mc,
+		Client:           client,
+	}
+
+	// Start the cycle of collecting and sending metrics
 	pollTicker := time.NewTicker(time.Duration(allflags.GetPollInterval()) * time.Second)
 	reportTicker := time.NewTicker(time.Duration(allflags.GetReportInterval()) * time.Second)
 	baseURL := allflags.GetServerAddress()
@@ -20,11 +36,10 @@ func NewAgent() {
 		select {
 		// When the pollTicker ticks, we collect the metrics
 		case <-pollTicker.C:
-			clientmetrics.PollMetrics()
-			// When the reportTicker ticks, we send the metrics
+			ma.PollMetrics()
+		// When the reportTicker ticks, we send the metrics
 		case <-reportTicker.C:
-			clientmetrics.ReportMetrics(baseURL)
+			ma.ReportMetrics(baseURL)
 		}
 	}
-
 }
--- internal/handlers/client/report.go
+++ internal/handlers/client/report.go
@@ -7,21 +7,11 @@ import (
 
 	"github.com/go-resty/resty/v2"
 	"github.com/sirupsen/logrus"
-
-	"Vova4o/metrix/internal/config"
 )
 
-// HttpClient is an interface for making HTTP requests
-type RestClient interface {
-	R() *resty.Request
-}
-
-// reportMetrics sends the metrics to the server
-func ReportMetrics(baseURL string) {
-	client := resty.New()
-
+func (mc *MetricsAgent) ReportMetrics(baseURL string) {
 	// Add a middleware logger
-	client.OnBeforeRequest(func(client *resty.Client, request *resty.Request) error {
+	mc.Client.OnBeforeRequest(func(client *resty.Client, request *resty.Request) error {
 		logrus.WithFields(logrus.Fields{
 			"url": request.URL,
 		}).Info("Sending request")
@@ -29,7 +19,7 @@ func ReportMetrics(baseURL string) {
 		return nil
 	})
 
-	client.OnAfterResponse(func(client *resty.Client, response *resty.Response) error {
+	mc.Client.OnAfterResponse(func(client *resty.Client, response *resty.Response) error {
 		logrus.WithFields(logrus.Fields{
 			"status": response.StatusCode(),
 			"body":   response.String(),
@@ -41,23 +31,23 @@ func ReportMetrics(baseURL string) {
 	errs := make(chan error)
 	var wg sync.WaitGroup
 
-	for metricName, metricValue := range config.GaugeMetrics {
+	for metricName, metricValue := range mc.GaugeMetrics {
 		wg.Add(1)
 		go func(name string, value float64) {
 			defer wg.Done()
-			err := SendMetric(client, "gauge", name, value, baseURL)
+			err := SendMetric(mc.Client, "gauge", name, value, baseURL)
 			if err != nil {
 				log.Printf("error sending gauge metric %s: %v", name, err)
 				errs <- fmt.Errorf("error sending gauge metric %s: %v", name, err)
 			}
-		}(metricName, float64(metricValue))
+		}(metricName, metricValue)
 	}
 
-	for metricName, metricValue := range config.CounterMetrics {
+	for metricName, metricValue := range mc.CounterMetrics {
 		wg.Add(1)
-		go func(name string, value int64) {
+		go func(name string, value int) {
 			defer wg.Done()
-			err := SendMetric(client, "counter", name, float64(value), baseURL)
+			err := SendMetric(mc.Client, "counter", name, float64(value), baseURL)
 			if err != nil {
 				log.Printf("error sending counter metric %s: %v", name, err)
 				errs <- fmt.Errorf("error sending counter metric %s: %v", name, err)
@@ -77,3 +67,78 @@ func ReportMetrics(baseURL string) {
 		fmt.Println(err)
 	}
 }
+
+// package clientmetrics
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"sync"
+
+// 	"github.com/go-resty/resty/v2"
+// 	"github.com/sirupsen/logrus"
+
+// 	"Vova4o/metrix/internal/config"
+// )
+
+// // reportMetrics sends the metrics to the server
+// func ReportMetrics(baseURL string) {
+// 	client := resty.New()
+
+// 	// Add a middleware logger
+// 	client.OnBeforeRequest(func(client *resty.Client, request *resty.Request) error {
+// 		logrus.WithFields(logrus.Fields{
+// 			"url": request.URL,
+// 		}).Info("Sending request")
+
+// 		return nil
+// 	})
+
+// 	client.OnAfterResponse(func(client *resty.Client, response *resty.Response) error {
+// 		logrus.WithFields(logrus.Fields{
+// 			"status": response.StatusCode(),
+// 			"body":   response.String(),
+// 		}).Info("Received response")
+
+// 		return nil
+// 	})
+
+// 	errs := make(chan error)
+// 	var wg sync.WaitGroup
+
+// 	for metricName, metricValue := range config.GaugeMetrics {
+// 		wg.Add(1)
+// 		go func(name string, value float64) {
+// 			defer wg.Done()
+// 			err := SendMetric(client, "gauge", name, value, baseURL)
+// 			if err != nil {
+// 				log.Printf("error sending gauge metric %s: %v", name, err)
+// 				errs <- fmt.Errorf("error sending gauge metric %s: %v", name, err)
+// 			}
+// 		}(metricName, float64(metricValue))
+// 	}
+
+// 	for metricName, metricValue := range config.CounterMetrics {
+// 		wg.Add(1)
+// 		go func(name string, value int64) {
+// 			defer wg.Done()
+// 			err := SendMetric(client, "counter", name, float64(value), baseURL)
+// 			if err != nil {
+// 				log.Printf("error sending counter metric %s: %v", name, err)
+// 				errs <- fmt.Errorf("error sending counter metric %s: %v", name, err)
+// 			}
+// 		}(metricName, metricValue)
+// 	}
+
+// 	// Close the errs channel after all goroutines have finished
+// 	go func() {
+// 		wg.Wait()
+// 		close(errs)
+// 	}()
+
+// 	// Print all errors
+// 	for err := range errs {
+// 		log.Println(err)
+// 		fmt.Println(err)
+// 	}
+// }
